a226e686158cb84eebe41a345b44c6bbe98de8fa
Unknown <kaguefb@gmail.com>
2018-04-23 19:27:15 +0200

    Ajout portlet with configuration
    Création par defaut du portlet MVC via IDE Liferay

---
+++ workspace/Kraken/modules/Portlet-with-config-panel/src/main/java/com/client/projet/portlet/constants/PortletWithConfigPanelPortletKeys.java
@@ -0,0 +1,13 @@
+package com.client.projet.portlet.constants;
+
+/**
+ * @author Kague
+ */
+public class PortletWithConfigPanelPortletKeys {
+
+	private PortletWithConfigPanelPortletKeys() {
+	}
+	
+	public static final String PortletWithConfigPanel = "PortletWithConfigPanel";
+
+}
---
+++ workspace/Kraken/modules/Portlet-with-config-panel/src/main/java/com/client/projet/portlet/portlet/PortletWithConfigPanelPortlet.java
@@ -0,0 +1,29 @@
+package com.client.projet.portlet.portlet;
+
+import com.client.projet.portlet.constants.PortletWithConfigPanelPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Kague
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=clientName",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Portlet-with-config-panel",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PortletWithConfigPanelPortletKeys.PortletWithConfigPanel,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PortletWithConfigPanelPortlet extends MVCPortlet {
+}
